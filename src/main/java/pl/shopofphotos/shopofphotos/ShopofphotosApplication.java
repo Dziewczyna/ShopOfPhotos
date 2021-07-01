f47286654c7d84ecb29ae064a2657db4af4f4860
Dziewczyna <joanna.podkoscielna@gmail.com>
2021-07-01 18:57:14 +0200

    Initial commit

---
+++ src/main/java/pl/shopofphotos/shopofphotos/ShopofphotosApplication.java
@@ -0,0 +1,13 @@
+package pl.shopofphotos.shopofphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopofphotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopofphotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/shopofphotos/shopofphotos/ShopofphotosApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.shopofphotos.shopofphotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopofphotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
